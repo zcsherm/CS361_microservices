7ad59e3e73d695c3db53d518ad9da50d5aa15e2d
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-08-06 10:21:23 -0700

    Create microservice_a.py

---
+++ microservice_a.py
@@ -0,0 +1,12 @@
+# Microservice A
+# Reads from a text file
+# Fetches a file
+# writes that file/file_location to the text file
+#
+# reads from text file
+# Gets a new image from text file
+# Updates the requested file with the new image
+
+class Microservice:
+    def __init__(self):
+        pass
