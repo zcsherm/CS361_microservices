aac75a8ed2166d9d0f6120d264c9365bead22da9
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-08-06 11:54:31 -0700

    Update microservice_a.py

--- microservice_a.py
+++ microservice_a.py
@@ -18,16 +18,20 @@ REQUESTS = [FETCH_REQUEST, WRITE_REQUEST]
 PASSED_READ = "ACCEPT"
 FAILED_READ = "INVALID"
 
-CANT_FIND = "404"
+NOT_FOUND = "404"
 TIMED_OUT = "408"
+
+IMAGE_DIRECTORY = 'card_image/'
+i
 
 class Microservice:
     def __init__(self):
         self.last_write = None
+        self.make_image_directory()
         self.main()
-
+        
     def main(self):
-        while True:
+        while self.last_write != QUIT_REQUEST:
             time.sleep(1)
             with open(PIPE, "r") as file:
                 content = file.read().upper()
@@ -37,21 +41,37 @@ class Microservice:
                 if content == WRITE_REQUEST:
                     self.overwrite_file()
                 if content == QUIT_REQUEST:
-                    return
+                    self.quit()
             elif content != self.last_write:
                 self.request_error(content)
 
     def fetch_file(self):
-        with open(PIPE, "w") as file:
-            file.write(PASSED_READ)
-        content = PASSED_READ
-        while content == PASSED_READ:
-            count = 0
+        self.write_to_pipe(PASSED_READ)
+        timer = 0
+        while content == self.last_write:
             time.sleep(.5)
             with open(PIPE, "r") as file:
                 content = file.read()
             if content != PASSED_READ:
-                content = content.lower().strip()
+                content = content.strip()
                 file = self.locate_file(content)
+                self.write_to_pipe(file)
+            if timer > 20:
+                self.write_to_pipe(TIMED_OUT)
+            timer += 1
+
+    def write_to_pipe(self, contents):
+        pass
+
+    def locate_file(self, query):
+        file_name = query.lower() + ".png"
+        file_path = os.path.abspath(IMAGE_DIRECTORY + file_name)
+        if not os.path.isfile(file_path):
+            file_path = NOT_FOUND
+        return file_path
+            
     def overwrite_file(self):
+        pass
+
+    def make_image_directory(self):
         pass
