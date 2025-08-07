968427fd8a15d3d8fb2c529ac9e4764d75b23e14
zcsherm <165740629+zcsherm@users.noreply.github.com>
2025-08-07 14:57:30 -0700

    Update microservice_b.py

--- microservice_b.py
+++ microservice_b.py
@@ -7,8 +7,10 @@ ADD_REQUEST = "ADD"
 REMOVE_REQUEST = "REMOVE"
 CREATE_REQUEST = "CREATE"
 QUIT_REQUEST = "QUIT"
+USER_SELECT = "USER"
 REQUESTS = [ADD_REQUEST, REMOVE_REQUEST, REMOVE_REQUEST, QUIT_REQUEST]
 
+REQUESTS = {ADD_REQUEST: }
 PASSED_READ = "PLAYER"
 WHICH_TEAM = "TEAM"
 FAILED_READ = "INVALID"
@@ -24,8 +26,15 @@ PLAYER_NAMES = "names.txt"
 
 class Microservice:
     def __init__(self):
+        self.requests = {
+                        ADD_REQUEST: self.add_player,
+                        REMOVE_REQUEST: self.remove_player,
+                        CREATE_REQUEST: self.add_new_player_to_database,
+                        QUIT_REQUEST: self.quit
+                        }
         self.make_pipe()
         self.make_player_directory()
+        self.open_master()
         self.last_write = None
         self.main()
         
@@ -35,7 +44,8 @@ class Microservice:
             time.sleep(1)
             with open(PIPE, "r") as file:
                 content = file.read().upper()
-
+            if content != '':
+                self.route_request(content)
         
     def make_pipe(self):
         if not os.path.isfile(path):
@@ -61,19 +71,36 @@ class Microservice:
                     "current_team": None
                    }
             players[player_names] = Player(data)
-        with open(MASTER_FILE, 'wb') as f:
-            pickle.dump(players, f)
+        with open(MASTER_FILE, 'wb') as file:
+            pickle.dump(players, file)
+
+    def open_master(self):
+        with open(MASTER_FILE, 'rb') as file:
+            self.players = pickle.load(file)
+
+    def send_and_wait_for_message(self, sent_message, response_function):
+        self.write_to_pipe(sent_message)
+        count = 0
+        while count < 20:
+            time.sleep(.5)
+            with open(PIPE, "r") as file:
+                content = file.read()
+            if content != '':
+                response_function(content)
+                
+    def choose_user(self):
+        self.send_and_wait_for_message(USER_SELECT,
+    def add_player(self):
+        
             
-    def validate_player_exists(self):
+    
+    def validate_player_exists(self, player):
         pass
 
     def validate_player_available(self):
         pass
         
     def validate_player_on_team(self):
-        pass
-        
-    def add_player(self):
         pass
 
     def remove_player(self):
@@ -82,5 +109,20 @@ class Microservice:
     def add_new_player_to_database(self):
         pass
 
-    def route_request(self):
-        pass
+    def write_to_pipe(self, contents):
+        """
+        Writes a given message to the pipeline and saves what was written
+        :param contents: The message to be written
+        """
+        with open(PIPE, "w") as file:
+            file.write(contents)
+        self.last_write = contents
+        
+    def route_request(self, content):
+        try:
+            self.requests[content]()
+        except KeyError:
+            self.invalid_request()
+
+    def invalid_request(self):
+        self.write_to_pipe(FAILED_READ)
